68d3f72125f681cf908f7319cd9480e46d55dd70
Cody Lin <chengchihlin1@gmail.com>
2024-06-21 01:25:18 -0400

    New portfolio project added

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -6,34 +6,41 @@ const portfolio = () => {
   const projects = [
     { 
     title: "Online Food Ordering Site - 'Chow' ", 
-    description: "This is a cooperative full stack website where I developed the frontend UI using React and coordinated the connection to the backend built using MongoDB. This is deployed on Heroku", 
+    description: "This is a cooperative full stack website where I developed the frontend UI using React and coordinated the connection to the backend built using MongoDB. This is deployed on Heroku.", 
     image: '/Chow.png', 
     link: "https://chow-6cdd847b467b.herokuapp.com/"
   },
   { 
     title: "Fitness/Wellness Blog - WellMates", 
-    description: "This website was created with HTML, Javascript and CSS. This was a cooperative project with limited website functionality. I focused on building the interactive UI and APIs", 
+    description: "This website was created with HTML, Javascript and CSS. This was a cooperative project with limited website functionality. I focused on building the interactive UI and APIs.", 
     image: "/p1.jpg",
     link: "https://codyccl.github.io/Wellmates/"
   },
-  // { 
-  //   title: "Express Notepad", 
-  //   description: "This challenge had us use express to create a note taking app that saves and deletes notes", 
-  //   image: "/p3.png", 
-  //   link: "https://codyccl.github.io/Notepad-app-11/"
-  // },
   { 
     title: "Cody's Landing Page", 
-    description: "My landing page is built using React. It introduces users about me as a software developer and showcases my technical skills. This site is deployed through netlify. My website is constantly updated with new portfolio projects. Stay tuned!", 
+    description: "My landing page is built using React. It introduces users about me as a software developer and showcases my technical skills. This site is deployed through Netlify. *Be on the lookout for new portfolio projects!", 
     image: "/Lighthouse.jpg",
     link: "https://codylinportfolio.netlify.app"
   },
   { 
+    title: "Book Search Engine", 
+    description: "This MERN stack website I created allows users to search for google books and allows signed up users to favorite books in their account. This is deployed on Render.", 
+    image: "/p3.png", 
+    link: "https://booksearchapi-gwrj.onrender.com"
+  },
+  { 
     title: "Javascript quiz", 
-    description: "Using Javascript to create a timed quiz that tally correct responses", 
+    description: "Using Javascript to create a timed quiz that tally correct responses. Still a work in progress!", 
     image: "/p4.png",
     link: "https://codyccl.github.io/Code-Quiz-Challenge4/" 
   },
+  { 
+    title: "TBD", 
+    description: "Coming Soon", 
+    image: "/wip.png",
+    link: "" 
+  },
+
   ];
   return (
     <>
